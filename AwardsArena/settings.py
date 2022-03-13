2c6794b2b3e040676ee468695a4fa5aaa70cd98f
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-03-13 01:43:30 -0500

    Created the navbar

--- AwardsArena/settings.py
+++ AwardsArena/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -127,6 +127,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL='/static/'
-STATICFILES =[
+STATICFILES_DIRS =[
     os.path.join(BASE_DIR,"static")
 ]
